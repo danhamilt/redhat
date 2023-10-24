dbc30486e5bbe21c3662d63bcb8a60e58d6d2aba
Daniel Hamilton <danielhamilton@daniels-air.lan>
2023-10-24 18:36:08 -0400

    django

--- api/api/settings.py
+++ api/api/settings.py
@@ -76,6 +76,7 @@ import dj_database_url
 DATABASES = {
     'default': dj_database_url.config()
 }
+ALLOWED_HOSTS = ['*']
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
